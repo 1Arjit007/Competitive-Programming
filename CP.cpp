3fa748aeb98855049e1c38d54a5e6004b1fbf9b2
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2020-09-13 19:40:31 +0530

    Updated

--- CP.cpp
+++ CP.cpp
@@ -5,3 +5,23 @@ Thus, it is not needed to separately include libraries such as iostream, vector,
   int main() {
   // solution comes here
   }
+
+//2.Input and output is sometimes a bottleneck in the program.
+//The following lines at the beginning of the code make input and output more efficient:
+   #include<bits/stdc++.h>
+   using namespace std;
+   int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // solution comes here
+   }  
+
+// 3.Sometimes the program should read a whole input line, possibly containing spaces.
+// This can be accomplished by using the getline function:
+   string s;
+  getline(cin, s);
+
+//4.If the amount of data is unknown, the following loop is useful:
+  while (cin >> x) {
+    // code
+  }
