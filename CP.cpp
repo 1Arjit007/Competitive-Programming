472a502d0c144e104dba6cbb926a0a6448af1cb2
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2020-09-13 19:31:10 +0530

    Create CP.cpp

---
+++ CP.cpp
@@ -0,0 +1,7 @@
+/*1.The #include line at the beginning of the code is a feature of the g++ compiler that allows us to include the entire standard library.
+Thus, it is not needed to separately include libraries such as iostream, vector, and algorithm, but rather theyare available automatically.*/
+  #include<bits/stdc++.h>
+  using namespace std;
+  int main() {
+  // solution comes here
+  }
